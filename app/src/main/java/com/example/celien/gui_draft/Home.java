ca7db63f9fb76819c1f2e85b9f8c6b346ba7cc6a
Celien <cesliens@gmail.com>
2015-02-18 17:04:54 +0100

    Set all the necessary stuff to use the Swipe Tabs

--- app/src/main/java/com/example/celien/gui_draft/Home.java
+++ app/src/main/java/com/example/celien/gui_draft/Home.java
@@ -1,19 +1,50 @@
 package com.example.celien.gui_draft;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+import com.example.celien.gui_draft.adapter.ViewPagerAdapter;
+import com.example.celien.gui_draft.tabs.SlidingTabLayout;
+
+
 public class Home extends ActionBarActivity {
+
+    Toolbar toolbar;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence titles[];
+    int nbTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
     }
 
+    private void init(){
+        nbTabs = 3;
+        titles = new CharSequence[]{"Cars", "Offers", "Account"};
+        toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(), titles, nbTabs);
+        pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+        tabs = (SlidingTabLayout)findViewById(R.id.tabs);
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+        tabs.setViewPager(pager);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
