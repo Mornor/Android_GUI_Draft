f8a34c78e6f2f2625cb9fb108ff734ddf5b66eac
Celien <cesliens@gmail.com>
2015-02-20 11:36:38 +0100

    Fix some bugs with the reachability of TextView inside Fragment

--- app/src/main/java/com/example/celien/gui_draft/tabs/TabPersonnalSpace.java
+++ app/src/main/java/com/example/celien/gui_draft/tabs/TabPersonnalSpace.java
@@ -4,9 +4,12 @@ package com.example.celien.gui_draft.tabs;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.celien.gui_draft.R;
 
@@ -16,7 +19,24 @@ public class TabPersonnalSpace extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_personnal_space, container, false);
 
+        TextView tvSettings = (TextView)rootView.findViewById(R.id.tvSettings);
+        TextView tvLogout = (TextView)rootView.findViewById(R.id.tvLogout);
+
+
+        tvSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvLogout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Logout", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return rootView;
     }
-
 }
